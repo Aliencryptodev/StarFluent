21e65fdd3a24165226dc5ee63b4a9d73af555c58
Aliencryptodev <aliencryptodev@gmail.com>
2025-08-26 01:52:30 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function HomePage() {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl">StarCraft Tower Defense</h1>
-    </div>
-  );
+  return <div>Test Page</div>;
 }
